963989d3038e196c4923b3bceb5da0ad2b77481c
TalonChen <talonc@nycstudents.net>
2025-12-18 21:16:51 -0500

    ea

--- clash/vite.config.js
+++ clash/vite.config.js
@@ -3,3 +3,16 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [tailwindcss()],
 });
+
+const URL = "https://api.clashroyale.com/v1/cards";
+
+async function getData(URL) {
+  try {
+    const response = await fetch(URL);
+    const data = await response.json(); //Turns Json into JS
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+getData(URL);
